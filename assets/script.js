95cb94fce3e4fa419c3ff57f01b87222cb7a9762
Maciej Molenda <maciekm777@gmail.com>
2017-06-19 08:46:44 +0200

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -98,3 +98,155 @@ $(document).ready(function(){
       $link.data('lockedAt', +new Date());		
 	});
 });
+
+//korekcja kaflów, które zostawiają pustą przestrzeń
+$(document).ready(function(){ 
+	$('.a3').click(function(){ 
+		if($(window).width() > 752 && $('.a1').hasClass('col-xs-6 col-sm-4') && $('.a2').hasClass('col-xs-6 col-sm-4') && $('.a3').hasClass('col-xs-12 col-sm-8')){	
+			$('.a2').insertAfter('.a3');
+		} else {	
+			$('.a3').insertAfter('.a2');
+		}
+	});	
+	$('.a6').click(function(){ 
+		if($(window).width() > 752 && $('.a4').hasClass('col-xs-6 col-sm-4') && $('.a5').hasClass('col-xs-6 col-sm-4') && $('.a6').hasClass('col-xs-12 col-sm-8')){	
+			$('.a5').insertAfter('.a6');
+		} else {	
+			$('.a6').insertAfter('.a5');
+		}
+	});	
+	
+	$('.a2').click(function(){ 
+		if($(window).width() < 752 && $('.a1').hasClass('col-xs-6 col-sm-4') && $('.a2').hasClass('col-xs-6 col-sm-4')){	
+			$('.a1').insertAfter('.a2');
+		} else {	
+			$('.a2').insertAfter('.a1');
+		}
+	});	
+	$('.a4').click(function(){ 
+		if($(window).width() < 752 && $('.a1').hasClass('col-xs-6 col-sm-4') && $('.a2').hasClass('col-xs-6 col-sm-4') &&  $('.a3').hasClass('col-xs-6 col-sm-4') && $('.a4').hasClass('col-xs-6 col-sm-4')){	
+			$('.a3').insertAfter('.a4');
+		} else {	
+			$('.a4').insertAfter('.a3');
+		}
+	});	
+});
+
+//odświeżanie strony przy zmianie wielkości okna
+$(document).ready(function(){ 
+	$(window).resize(function(){location.reload();});
+});	
+
+//dodawanie kafli po zjechaniu na sam dół strony
+$(window).scroll(function() {
+    if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+        $('.hiddenn').appendTo('.row').show("slow"); 		   
+    } 
+	if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+	    $(window).scrollTop($(window).scrollTop()-19);
+    }
+});
+
+//dodanie nowych kafli po naciśnięciu przycisku Add
+$(document).ready(function(){ 
+	$('.dodaj').click(function(){
+		if($("input#name").val() != "" && $("input#subject").val() != "" && $("input#date").val() != ""){
+			$('#message').css('visibility','visible');
+			$(".row").append('<div class="tile nowy hiddenn col-xs-6 col-sm-4"><div class="img-responsive noweTlo" style="background-size:100% 50%;background-color:white;"><div class="img-foot text-left"><h5 class="termin">November 25, 2015</h5><a class="description" href="#" target="_blank">How to find design inspiration in the simple things around you</a></div><img class="foto" src="assets/images/user.png" alt="author"/></div></div>');
+			//ustawienie wybranego obrazu jako tła kafla
+			var $background = $('#blah').attr('src');
+			$('.noweTlo').css({
+				'background': 'url(' + $background + ') no-repeat center top',
+				'background-size':'100% 50%',
+				'background-color':'white',
+				});		
+			$( "div" ).removeClass( "noweTlo" );
+			//określenie wielkości liter opisu w zależności od wielkości ekranu
+			if($(window).width() > 960){
+				$('.nowy').css("font-size", 19 + "px");	
+			} else {
+				$('.nowy').find('h5').css("font-size", 10 + "px");
+				$('.nowy').css("font-size", 12 + "px");
+			};
+			$('.nowy').hide();
+	
+			//Ustawienie tekstu nowego kafla 
+			var $subject = $( "#subject" ).val();
+			var $date = $( "#date" ).val();
+			$('.description').html($subject);
+		    $('.termin').html($date);
+			$( "h5" ).removeClass( "termin" );
+			$( "a" ).removeClass( "description" );
+			
+		//schowanie panelu i wyświetlenie potwierdzenia wypełnienia formularza oraz stworzenia nowego kafla	
+		
+   		  $('.description').html(subject);
+		  $('.termin').html(date);
+		  $('#contact_form').hide()
+		  $('#message').html("<h2>Form Submitted!</h2>")
+		  .append("<p>Your tile has been added.</p>")
+		  .hide()
+		  .fadeIn(1500)
+		}
+	});
+});
+
+//strzałka przekierowywująca na samą górę
+var amountScrolled = 300;
+
+$(window).scroll(function() {
+	if ( $(window).scrollTop() > amountScrolled ) {
+		$('#myBtn').fadeIn('slow');
+	} else {
+		$('#myBtn').fadeOut('slow');
+	}
+});
+
+//przekierowanie na górę po kliknięciu na strzałkę
+$(document).on('click','#myBtn', function(){
+	$('html, body').animate({
+		scrollTop: 0
+	}, 700);
+	return false;
+});
+
+//rozwinięcie bazy po kliknięciu na przycisk Add new tile
+$(document).on('click','.rozwinBaze', function(){
+	$('.baza').css('display','block')
+	$('#contact_form').show();
+	$('#message').css('visibility','hidden');
+});
+
+//zwinięcie bazy po kliknięciu na przycisk Finish
+$(document).on('click','.zakoncz', function(){
+	$('.baza').css('display','none')
+});
+
+//pobranie tekstu z pól formularza i wysłanie do nowych kafli
+$(function() {
+    $('.error').hide();
+    $(".button").click(function() {
+      // walidacja i przetworzenie formularza     
+      $('.error').hide();
+  	  var name = $("input#name").val();
+  		if (name == "") {
+        $("label#name_error").show();
+        $("input#name").focus();
+        return false;
+      }
+  		var subject = $("input#subject").val();
+  		if (subject == "") {
+        $("label#subject_error").show();
+        $("input#subject").focus();
+        return false;
+      }
+  		var date = $("input#date").val();
+  		if (date == "") {
+        $("label#date_error").show();
+        $("input#date").focus();
+        return false;
+      }
+      var dataString = 'name='+ name + '&subject=' + subject + '&date=' + date;	  
+	  return false;
+    });
+});
